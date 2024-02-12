d46c67f23c1d1971c467641f0f38aa4b57568ec5
Manvendra Singh <08singhmanvendra@gmail.com>
2024-02-12 11:20:51 +0530

    add new questions

--- nestedloop.js
+++ nestedloop.js
@@ -1,163 +1,209 @@
 // 1.
-for(var a=1;a<5;a++){
-	for(var x=1;x<5;x++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x < 5; x++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 2.
-for(var a=1;a<5;a++){
-	for(var x=1;x<5;x++){
-		document.write(a+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x < 5; x++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 3.
-for(var a=1;a<5;a++){
-	for(var x=1;x<5;x++){
-		document.write(x+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x < 5; x++) {
+    document.write(x + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 4.
-for(var a=1;a<5;a++){
-	for(var x=4;x>0;x--){
-		document.write(x+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 4; x > 0; x--) {
+    document.write(x + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 5.
-for(var a=4;a>0;a--){
-	for(var x=1;x<5;x++){
-		document.write(a+' ');
-	}
-	document.write('<br>');
+for (var a = 4; a > 0; a--) {
+  for (var x = 1; x < 5; x++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 6.
-for(var a=1;a<5;a++){
-	for(var x=1;x<=a;x++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x <= a; x++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 7.
-for(var a=1;a<5;a++){
-	for(var x=1;x<=a;x++){
-		document.write(x+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x <= a; x++) {
+    document.write(x + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 8.
-for(var a=1;a<5;a++){
-	for(var x=1;x<=a;x++){
-		document.write(a+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x <= a; x++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 9.
-for(var a=1;a<8;a=a+2){
-	for(var x=1;x<=a;x++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 8; a = a + 2) {
+  for (var x = 1; x <= a; x++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 10.
-for(var a=4;a>0;a--){
-	for(var x=1;x<=a;x++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 4; a > 0; a--) {
+  for (var x = 1; x <= a; x++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 11.
-for(var a=4;a>0;a--){
-	for(var x=1;x<=a;x++){
-		document.write(a+' ');
-	}
-	document.write('<br>');
+for (var a = 4; a > 0; a--) {
+  for (var x = 1; x <= a; x++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 12.
-for(var a=4;a>0;a--){
-	for(var x=1;x<=a;x++){
-		document.write(x+' ');
-	}
-	document.write('<br>');
+for (var a = 4; a > 0; a--) {
+  for (var x = 1; x <= a; x++) {
+    document.write(x + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 13.
-for(var a=1;a<5;a++){
-	for(var x=a;x<4;x++){
-		document.write('$ ');
-	}
-	for(var i=1;i<=a;i++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = a; x < 4; x++) {
+    document.write("$ ");
+  }
+  for (var i = 1; i <= a; i++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 14.
-for(var a=1;a<5;a++){
-	for(var x=a;x<4;x++){
-		document.write('$ ');
-	}
-	for(var i=1;i<=a;i++){
-		document.write(i+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = a; x < 4; x++) {
+    document.write("$ ");
+  }
+  for (var i = 1; i <= a; i++) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 15.
-for(var a=1;a<5;a++){
-	for(var x=a;x<4;x++){
-		document.write('$ ');
-	}
-	for(var i=1;i<=a;i++){
-		document.write(a+' ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = a; x < 4; x++) {
+    document.write("$ ");
+  }
+  for (var i = 1; i <= a; i++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 21.
-for(var a=1;a<5;a++){
-	for(var x=1;x<=a;x++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = 1; x <= a; x++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-for(var i=3;i>0;i--){
-		for(var n=1;n<=i;n++){
-			document.write('x ');
-	}
-	document.write('<br>');
+for (var i = 3; i > 0; i--) {
+  for (var n = 1; n <= i; n++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
 // 22.
-for(var a=1;a<5;a++){
-	for(var x=a;x<4;x++){
-		document.write('$ ');
-	}
-	for(var i=1;i<=a;i++){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 1; a < 5; a++) {
+  for (var x = a; x < 4; x++) {
+    document.write("$ ");
+  }
+  for (var i = 1; i <= a; i++) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-for(var a=3;a>0;a--){
-	for(var x=1;x<3;x++){
-		document.write('$ ');
-	}
-	for(var i=a;i>0;i--){
-		document.write('x ');
-	}
-	document.write('<br>');
+for (var a = 3; a > 0; a--) {
+  for (var x = 1; x < 3; x++) {
+    document.write("$ ");
+  }
+  for (var i = a; i > 0; i--) {
+    document.write("x ");
+  }
+  document.write("<br>");
 }
-document.write('<br>');
+document.write("<br>");
+// 30.
+for (a = 1; a < 6; a++) {
+  for (x = 1; x <= a - 1; x++) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= 6 - a; i++) {
+    document.write("x ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 37.
+for (a = 1; a < 6; a++) {
+  for (i = 1; i <= 6 - a; i++) {
+    document.write(a + " ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 38.
+for (a = 5; a > 0; a--) {
+  for (i = 5; i > 5 - a; i--) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 39.
+for (a = 5; a > 0; a--) {
+  for (i = 5; i >= a; i--) {
+    document.write(i + " ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 16.
+// for(a=4;a>0;a--){
+// 	for(i=1;i<=a-1;i++){
+// 		document.write('$ ');
+// 	}
+// 	for(x=a;x>=a;x--){
+// 		document.write('x ');
+// 	}
+// 	document.write('<br>');
+// }
+// document.write('<br>');
