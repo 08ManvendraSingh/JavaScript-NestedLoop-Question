1e4f06e72ccd93af3d9232a271b2d906977a2efa
Manvendra Singh <08singhmanvendra@gmail.com>
2024-02-14 11:27:31 +0530

    add more questions

--- nestedloop.js
+++ nestedloop.js
@@ -127,6 +127,50 @@ for (var a = 1; a < 5; a++) {
   document.write("<br>");
 }
 document.write("<br>");
+// 16.
+for (a = 1; a <= 4; a++) {
+  for (i = 4 - a; i > 0; i--) {
+    document.write("$ ");
+  }
+  for (j = 1; j <= a; j++) {
+    document.write("*" + " " + "1");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 17.
+for (a = 1; a < 9; a = a + 2) {
+  for (i = 9 - a; i > 0; i--) {
+    document.write("$ ");
+  }
+  for (x = 1; x <= a; x++) {
+    document.write("x " + "1 ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 18.
+for (a = 1; a < 9; a = a + 2) {
+  for (i = 9 - a; i > 0; i--) {
+    document.write("$ ");
+  }
+  for (x = 1; x <= a; x++) {
+    document.write(x + " ! ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 20.
+for (a = 1; a < 9; a = a + 2) {
+  for (i = 9 - a; i > 0; i--) {
+    document.write("$ ");
+  }
+  for (x = 1; x <= a; x++) {
+    document.write(a + " 1 ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
 // 21.
 for (var a = 1; a < 5; a++) {
   for (var x = 1; x <= a; x++) {
@@ -157,6 +201,61 @@ for (var a = 3; a > 0; a--) {
   }
   for (var i = a; i > 0; i--) {
     document.write("x ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 23.
+for (a = 7; a > 0; a = a - 2) {
+  for (x = 7 - a; x > 0; x--) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= a; i++) {
+    document.write("x " + "! ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 24.
+for (a = 7; a > 0; a = a - 2) {
+  for (x = 7 - a; x > 0; x--) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= a; i++) {
+    document.write(i + " ! ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 25.
+for (a = 7; a > 0; a = a - 2) {
+  for (x = 7 - a; x > 0; x--) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= a; i++) {
+    document.write(1 + " ! ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 26.
+for (a = 5; a > 0; a = a - 2) {
+  for (x = 5 - a; x > 0; x--) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= a; i++) {
+    document.write(5 + " ! ");
+  }
+  document.write("<br>");
+}
+document.write("<br>");
+// 27.
+for (a = 5; a > 0; a--) {
+  for (x = 5 - a; x > 0; x--) {
+    document.write("$ ");
+  }
+  for (i = 1; i <= a; i++) {
+    document.write("x " + "! ");
   }
   document.write("<br>");
 }
@@ -196,14 +295,3 @@ for (a = 5; a > 0; a--) {
   document.write("<br>");
 }
 document.write("<br>");
-// 16.
-// for(a=4;a>0;a--){
-// 	for(i=1;i<=a-1;i++){
-// 		document.write('$ ');
-// 	}
-// 	for(x=a;x>=a;x--){
-// 		document.write('x ');
-// 	}
-// 	document.write('<br>');
-// }
-// document.write('<br>');
